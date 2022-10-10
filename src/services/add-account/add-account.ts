747ee082ac769fb01fde67485112ee52ca7d1fb0
Jhon Santana <jhonsstn@gmail.com>
2022-10-10 20:29:13 -0300

    feat(DbAddAccount): should call AddAccountRepository with correct values

--- src/services/add-account/add-account.ts
+++ src/services/add-account/add-account.ts
@@ -1,12 +1,20 @@
 import { AccountModel } from '../../models/account';
+import AddAccountRepository from '../../repositories/interfaces/add-account-repository';
 import Encrypter from '../encrypter/encrypter-interface';
 import AddAccount, { AddAccountModel } from './add-account-interface';
 
 export default class DbAddAccount implements AddAccount {
-  constructor(private readonly encrypter: Encrypter) {}
+  constructor(
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository,
+  ) {}
 
-  // Omit<AccountModel, 'password'>
-  async add(account: AddAccountModel): Promise<any> {
-    await this.encrypter.encrypt(account.password);
+  async add(account: AddAccountModel): Promise<Omit<AccountModel, 'password'>> {
+    const encryptedPassword = await this.encrypter.encrypt(account.password);
+    const newAccount = await this.addAccountRepository.add({
+      ...account,
+      password: encryptedPassword,
+    });
+    return newAccount;
   }
 }
