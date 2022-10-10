52443492a45f2703e41dcbdf4bb00caa2e969584
Jhon Santana <jhonsstn@gmail.com>
2022-10-10 20:10:14 -0300

    test(DbAddAccount): should call Encrypter with correct password

---
+++ src/services/add-account/add-account.spec.ts
@@ -0,0 +1,52 @@
+import { AccountModel } from '../../models/account';
+import Encrypter from '../encrypter/encrypter-interface';
+import DbAddAccount from './add-account';
+import { AddAccountModel } from './add-account-interface';
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  document: 'valid_document',
+  password: 'encrypted_password',
+});
+
+const makeFakeAccountData = (): AddAccountModel => ({
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  document: 'valid_document',
+  password: 'valid_password',
+});
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    encrypt(_password: string): Promise<string> {
+      return Promise.resolve('encrypted_password');
+    }
+  }
+  return new EncrypterStub();
+};
+
+interface SutTypes {
+  sut: DbAddAccount;
+  encrypterStub: Encrypter;
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter();
+  const sut = new DbAddAccount(encrypterStub);
+  return {
+    sut,
+    encrypterStub,
+  };
+};
+
+describe('AddAccount', () => {
+  it('should call Encrypter with correct password', () => {
+    const { sut, encrypterStub } = makeSut();
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+    const accountData = makeFakeAccountData();
+    sut.add(accountData);
+    expect(encryptSpy).toHaveBeenCalledWith(accountData.password);
+  });
+});
