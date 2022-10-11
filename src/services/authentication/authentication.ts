9ba265b5af0da2ea3b5fa5e1b2d3cc32a0f57030
Jhon Santana <jhonsstn@gmail.com>
2022-10-11 14:53:38 -0300

    feat(Authentication): should call HashComparer with correct values

--- src/services/authentication/authentication.ts
+++ src/services/authentication/authentication.ts
@@ -1,16 +1,24 @@
 import GetAccountByEmailRepository from '../../repositories/get-account-by-email-repository';
+import HashComparer from '../hash-comparer/hash-comparer-interface';
 import { AuthenticationParams } from './authentication-interface';
 
 export default class Authentication implements Authentication {
   constructor(
     private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
+    private readonly hashComparerStub: HashComparer,
   ) {}
 
   // TODO: change any return
   async auth(authenticationParams: AuthenticationParams): Promise<any> {
-    await this.getAccountByEmailRepository.getAccountByEmail(
+    const account = await this.getAccountByEmailRepository.getAccountByEmail(
       authenticationParams.email,
     );
+    if (account) {
+      await this.hashComparerStub.compare(
+        authenticationParams.password,
+        account.password,
+      );
+    }
 
     return null;
   }
