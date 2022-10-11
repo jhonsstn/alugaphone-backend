1c5ccff5e14a9011dcd707fd76913c58469dcf97
Jhon Santana <jhonsstn@gmail.com>
2022-10-11 17:13:24 -0300

    feat(BcryptAdapter): should return true if compare succeeds

--- src/services/bcrypt/bcrypt-adapter.ts
+++ src/services/bcrypt/bcrypt-adapter.ts
@@ -1,11 +1,17 @@
 import bcrypt from 'bcrypt';
+import HashComparer from './hash-comparer-interface';
 import Hasher from './hasher-interface';
 
-export default class BcryptAdapter implements Hasher {
+export default class BcryptAdapter implements Hasher, HashComparer {
   constructor(private readonly salt: number) {}
 
   async encrypt(password: string): Promise<string> {
     const hashedPassword = await bcrypt.hash(password, this.salt);
     return hashedPassword;
+  }
+
+  async compare(password: string, hashedPassword: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(password, hashedPassword);
+    return isValid;
   }
 }
