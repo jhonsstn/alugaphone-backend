97c0d1b2fb93301e226cf733e2dc6a506accdbf7
Jhon Santana <jhonsstn@gmail.com>
2022-10-11 14:07:58 -0300

    feat(Authentication): should call GetAccountByEmailRepository with correct email

---
+++ src/repositories/get-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../models/account';
+
+export default interface GetAccountByEmailRepository {
+  getAccountByEmail(email: string): Promise<AccountModel>;
+}
---
+++ src/services/authentication/authentication.ts
@@ -0,0 +1,15 @@
+import GetAccountByEmailRepository from '../../repositories/get-account-by-email-repository';
+import { AuthenticationParams } from './authentication-interface';
+
+export default class Authentication implements Authentication {
+  constructor(
+    private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
+  ) {}
+
+  // TODO: change any return
+  async auth(authenticationParams: AuthenticationParams): Promise<any> {
+    this.getAccountByEmailRepository.getAccountByEmail(
+      authenticationParams.email,
+    );
+  }
+}
