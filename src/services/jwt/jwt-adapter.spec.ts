4717e21fffd531c60114d52c0adccf50ab4e944d
Jhon Santana <jhonsstn@gmail.com>
2022-10-13 14:37:48 -0300

    test(JwtAdapter): test decrypter method

--- src/services/jwt/jwt-adapter.spec.ts
+++ src/services/jwt/jwt-adapter.spec.ts
@@ -1,9 +1,16 @@
 import jwt from 'jsonwebtoken';
 import JwtAdapter from './jwt-adapter';
+import { EncrypterParams } from './jwt-encrypt-interface';
 
 jest.mock('jsonwebtoken', () => ({
-  async sign(): Promise<string> {
-    return Promise.resolve('any_token');
+  sign(): string {
+    return 'any_token';
+  },
+  verify(): EncrypterParams {
+    return {
+      id: 'any_id',
+      email: 'any_email@mail.com',
+    };
   },
 }));
 
@@ -31,5 +38,30 @@ describe('JwtAdapter', () => {
     });
     const promise = sut.encrypt({ id: 'any_id', email: 'any_email' });
     await expect(promise).rejects.toThrow();
+  });
+
+  it('should call verify with correct values', async () => {
+    const sut = new JwtAdapter('secret');
+    const verifySpy = jest.spyOn(jwt, 'verify');
+    await sut.decrypt('any_token');
+    expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
+  });
+
+  it('should return a value on verify success', async () => {
+    const sut = new JwtAdapter('secret');
+    const value = await sut.decrypt('any_token');
+    expect(value).toEqual({
+      id: 'any_id',
+      email: 'any_email@mail.com',
+    });
+  });
+
+  it('should return null if verify throws', async () => {
+    const sut = new JwtAdapter('secret');
+    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const value = await sut.decrypt('any_token');
+    expect(value).toBeNull();
   });
 });
