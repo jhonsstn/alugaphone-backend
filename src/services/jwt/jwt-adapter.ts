7f85357f26d45d59586fb7b474da3de48664a040
Jhon Santana <jhonsstn@gmail.com>
2022-10-13 14:37:20 -0300

    feat(JwtAdapter): add decrypter method

--- src/services/jwt/jwt-adapter.ts
+++ src/services/jwt/jwt-adapter.ts
@@ -1,11 +1,20 @@
 import jwt from 'jsonwebtoken';
-import { Encrypter, EncrypterParams } from './jwt-encrypt-interface';
+import { Decrypter, Encrypter, EncrypterParams } from './jwt-encrypt-interface';
 
-export default class JwtAdapter implements Encrypter {
+export default class JwtAdapter implements Encrypter, Decrypter {
   constructor(private readonly secret: string) {}
 
   async encrypt(tokenData: EncrypterParams): Promise<string> {
     const token = jwt.sign(tokenData, this.secret);
     return token;
+  }
+
+  async decrypt(token: string): Promise<EncrypterParams | null> {
+    try {
+      const tokenData = jwt.verify(token, this.secret) as EncrypterParams;
+      return tokenData;
+    } catch (error) {
+      return null;
+    }
   }
 }
